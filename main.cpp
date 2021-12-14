8bba1db128f7d38f01c9fcecfeae127bea55c52d
Shreya Anand <shreyaanand2908@gmail.com>
2021-12-14 21:30:42 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show(); //showing the main window
+    return a.exec(); //event loop
+}
